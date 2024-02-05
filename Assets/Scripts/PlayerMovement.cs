5b2085d7c582fab7dc9bf77eb6d3ff90ef313a79
Ulik <proigrok.craft@gmail.com>
2024-02-05 12:36:02 +0200

    Add gun, jump, crouch and sprint.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,26 +8,36 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 5f;
-
-    public float gravity = -9.81f;
-
-    public Transform groundCheck;
-
+    public float gravity = -50f;
+    public float jumpHeight = 3f;
     public float groundDistance = 0.4f;
 
+    public Transform groundCheck;
     public LayerMask groundMask;
-
     Vector3 velocity;
-
     bool isGrounded;
 
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 9f;
+        }
+        else
+        {
+            speed = 5f;
+        }
+
         if(isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
+        }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
         float x = Input.GetAxis("Horizontal");
